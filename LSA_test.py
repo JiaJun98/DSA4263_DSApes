655c2cd2c40ea2cd267970decc7bc177d8fd62a6
kuro1511 <tingyen@hotmail.com>
2023-02-22 20:24:14 +0800

    Testing commit

---
+++ LSA_test.py
@@ -0,0 +1,14 @@
+#LSA
+#Just some copy pasted stuff to test pushing
+# tfidf_vec = TfidfVectorizer(use_idf=True, norm='l2')
+# svd = TruncatedSVD(n_components=dim) #singular value decomposition
+# transformed_x_train = tfidf_vec.fit_transform(x_train)
+# transformed_x_test = tfidf_vec.transform(x_test)
+# print('TF-IDF output shape:', transformed_x_train.shape)
+# x_train_svd = svd.fit_transform(transformed_x_train)
+# x_test_svd = svd.transform(transformed_x_test)
+# print('LSA output shape:', x_train_svd.shape)
+# explained_variance = svd.explained_variance_ratio_.sum()
+# print("Sum of explained variance ratio: %d%%" % (int(explained_variance * 100)))
+def fn():
+    pass
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DSA4263_DSApes
+Testing if branch + push works
+Workflow
+EDA
+Preprocessing (Tokenize, stopword removal, TF-IDF (depend on model))
+Sentimental analysis (Use pretrained models or build from scratch)
+Topic modelling for positive and negative sentiment
+Visualisation (Sentiments, topics, LIME) -> Find trend and interpret it
+deploy model on REST API (Flask Or H2O?)
+Front-end (Leave for later)
+
+Historic and future trends (??)
