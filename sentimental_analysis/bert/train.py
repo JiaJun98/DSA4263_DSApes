011370aee0adc4aee634f06a85a7c4ed01b3cb74
JiaJun98 <gohjiajun.98@gmail.com>
2023-03-22 18:49:18 +0800

    Edit changes for better importing from parental directory

--- sentimental_analysis/bert/train.py
+++ sentimental_analysis/bert/train.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[15]:
 
 
 import os
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import torch
 from torch import nn
+import syspend
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score
@@ -26,8 +27,6 @@ from transformers import BertModel
 from transformers import AdamW, get_linear_schedule_with_warmup
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
 
-import sys
-sys.path.append("../..")
 from utility import parse_config, seed_everything, custom_print
 from preprocess_class import create_datasets
 from dataset import full_bert_data_loader,preprocessing_for_bert, create_data_loader, full_create_data_loader
@@ -37,17 +36,17 @@ from dataset import full_bert_data_loader,preprocessing_for_bert, create_data_lo
 # ##### DONE: Add Bert Dataset class (Inherit or create yourself)
 # ##### DONE: Run 1 iterations of BERT model and add the metrics measuring(This week lecture) in utility.py
 # ##### DONE: Add the config variables from yml
-# ##### PENDING: Remove some other data
+# ##### PENDING: Remove some other data(like links)
 # ##### PENDING: predictions
 # ##### PENDING: See results or plot graph when adjusting threshold
 
-# In[2]:
+# In[14]:
 
 
 #Creating BERTClassifier class
 
 
-# In[3]:
+# In[12]:
 
 
 def compute_metrics(eval_pred):
@@ -63,7 +62,7 @@ def compute_metrics(eval_pred):
     }
 
 
-# In[4]:
+# In[10]:
 
 
 #Trainer arguments
@@ -114,13 +113,12 @@ def train(model_name, train_dataset, eval_dataset):
 
 # #### Driver class
 
-# In[15]:
+# In[13]:
 
 
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     curr_dir = os.getcwd()
-    os.chdir("../..")
     config_path = os.path.join(curr_dir, 'bert_sentiment_config.yml')
     config_file = parse_config(config_path)
     model_name = config_file['model']['model_name']
@@ -130,36 +128,43 @@ if __name__ == "__main__":
     epochs = int(config_file['model']['epochs'])
     learning_rate = float(config_file['model']['learning_rate'])
     epsilon = float(config_file['model']['epsilon'])
-    train_file = config_file['model']['train_file']
-    data_folder = os.path.join(os.getcwd(), config_file['model']['data_path'])
+    train_file = config_file['model']['data_folder']
+    home_folder = os.path.abspath(os.path.join(os.getcwd(),'../..'))
     model_path = os.path.join(curr_dir, config_file['model']['model_path'])
     logging_path = os.path.join(curr_dir,config_file['model']['log_path'])
         
-    data_df = pd.read_csv(os.path.join(data_folder,train_file))
+    data_df = pd.read_csv(os.path.join(home_folder,train_file))
     logger = open(os.path.join(curr_dir, logging_path), 'w')
-    custom_print(f'Device availiable: {device}')
+    custom_print(f'Device availiable: {device}', logger = logger)
     train_df, test_df = train_test_split(data_df, test_size = 0.2, random_state = 4263) #You are using slighly different for BERT
     train_dataset, val_dataset = full_bert_data_loader(model_name,max_len, batch_size, True, train_df)
-    custom_print("Train_val dataset loaded")
-    custom_print('Training model')
+    custom_print("Train_val dataset loaded",logger = logger)
+    custom_print('Training model',logger = logger)
     seed_everything()
-    custom_print('---------------------------------\n')
+    custom_print('---------------------------------\n',logger = logger)
 
-    custom_print("Hyperparameters:")
-    custom_print(f"model name: {model_name}")
-    custom_print(f"Number of epochs: {epochs}")
-    custom_print(f"number of classes: {n_classes}")
-    custom_print(f"max length: {max_len}")
-    custom_print(f"batch size: {batch_size}")
-    custom_print(f"learning rate: {learning_rate}")
+    custom_print("Hyperparameters:",logger = logger)
+    custom_print(f"model name: {model_name}",logger = logger)
+    custom_print(f"Number of epochs: {epochs}",logger = logger)
+    custom_print(f"number of classes: {n_classes}",logger = logger)
+    custom_print(f"max length: {max_len}",logger = logger)
+    custom_print(f"batch size: {batch_size}",logger = logger)
+    custom_print(f"learning rate: {learning_rate}",logger = logger)
     
     trainer = train(model_name, train_dataset, val_dataset)
-    custom_print('Training complete!')
+    custom_print('Training complete!',logger = logger)
     
-    custom_print('Showing Training and Evaluation metrics....')
+    custom_print('Showing Training and Evaluation metrics....',logger = logger)
     #https://stackoverflow.com/questions/68806265/huggingface-trainer-logging-train-data
     for obj in trainer.state.log_history:
         for key,value in obj.items():
             custom_print(f'{key}: {value}')
     logger.close()
     
+
+
+# In[17]:
+
+
+get_ipython().system('jupyter nbconvert --to script train.ipynb')
+
