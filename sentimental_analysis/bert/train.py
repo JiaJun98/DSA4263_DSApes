87fa2edc0dfb52ebb9f57c2f9ab260f9ac8ada21
JiaJun98 <gohjiajun.98@gmail.com>
2023-03-26 18:27:02 +0800

    bert-large Sentimental Classifier(Non-Hugging Face Trainer Arguements) 91.8% Accuracy 0.5 threshold

--- sentimental_analysis/bert/train.py
+++ sentimental_analysis/bert/train.py
@@ -309,7 +309,7 @@ def compute_metrics(eval_pred):
         'f1': f1
     }
 
-# #### Driver class
+###### Driver class
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     curr_dir = os.getcwd()
