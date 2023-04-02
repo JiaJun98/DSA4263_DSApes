6bf484c10e4c5f67e78b2de19c4aee093147a527
JiaJun98 <gohjiajun.98@gmail.com>
2023-04-03 01:43:56 +0800

    Merging Version 1 flask to main branch

---
+++ flask/app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from flask import Flask, url_for, render_template, request
+import torch
+import torch.nn.functional as F
+import syspend
+import numpy as np
+import os
+from utility import parse_config
+from sentimental_analysis.bert.train import BertClassifier
+from sentimental_analysis.bert.dataset import single_data_loader
+
+
+app = Flask(__name__)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+config_path = os.path.join(os.getcwd(), 'flask_app_config.yml')
+config_file = parse_config(config_path)
+model_name = config_file['flask']['model_name']
+n_classes = int(config_file['flask']['num_classes'])
+max_len = int(config_file['flask']['max_len'])
+model_path = config_file['flask']['model_path']
+THRESHOLD = int(config_file['flask']['THRESHOLD'])
+MODEL =  BertClassifier(model_name, n_classes)
+MODEL.model.to(device)
+
+#Routes - Creating a simple route
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def predict(): #Send data from front-end to backend then to front end
+    if request.method == "POST":
+        # Get the form data
+        tweet = request.form["tweet"]
+        test_dataloader = single_data_loader(model_name, max_len, tweet)
+        preds = MODEL.predict(test_dataloader, THRESHOLD)
+        # Return the template with the form data
+        return render_template("index.html", tweet = tweet, preds = preds)
+
+
+if __name__ == '__main__':
+    app.run(debug = True, use_reloader = True)
+
+
+
+
