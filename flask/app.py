d50aaae901a5b0faedad4b715c676c809dfe8698
JiaJun98 <gohjiajun.98@gmail.com>
2023-04-04 01:17:22 +0800

    Changed threshold based on roberta-large; weights in local repo

--- flask/app.py
+++ flask/app.py
@@ -9,7 +9,7 @@ import numpy as np
 import os
 from utility import parse_config
 from sentimental_analysis.bert.train import BertClassifier
-from sentimental_analysis.non_bert.train import predict_data
+#from sentimental_analysis.non_bert.train import predict_data
 from sentimental_analysis.bert.dataset import single_data_loader
 
 
@@ -23,7 +23,7 @@ model_name = config_file['flask']['model_name']
 n_classes = int(config_file['flask']['num_classes'])
 max_len = int(config_file['flask']['max_len'])
 model_path = config_file['flask']['model_path']
-THRESHOLD = int(config_file['flask']['THRESHOLD'])
+THRESHOLD = float(config_file['flask']['THRESHOLD'])
 MODEL =  BertClassifier(model_name, n_classes) #Add non-bert also
 MODEL.model.to(device)
 
@@ -38,8 +38,6 @@ def predict(): #Send data from front-end to backend then to front end
     if request.method == "POST":
         # Get the form data
         tweet = request.form["tweet"]
-        #[[0.3976914  0.60230863]] Means positive
-        THRESHOLD = 0.8
         test_dataloader = single_data_loader(model_name, max_len, tweet)
         preds = MODEL.predict(test_dataloader, THRESHOLD)
         # Return the template with the form data
