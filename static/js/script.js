eb26d87dbcfcd2bf45e2d8504cc72c7c83856f3a
JiaJun98 <gohjiajun.98@gmail.com>
2023-04-11 21:44:11 +0800

    Adding empty text checker draft

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,20 @@
+window.onload = init;
+
+
+
+
+document.getElementById("my-form").addEventListener("submit", function(event) {
+  // Get the value of the textarea
+  var textAreaValue = document.getElementsByName("review")[0].value.trim();
+  // If the textarea is empty, prevent submission of the form
+  if (textAreaValue === "") {
+    event.preventDefault();
+    // Display an error message
+    document.getElementById("error-message").innerHTML = "Please enter some text.";
+  }
+});
+
+
 function checkInput() {
     var fileInput = document.getElementById("file");
     if (fileInput.value == "") {
