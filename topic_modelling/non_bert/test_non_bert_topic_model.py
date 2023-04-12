a2e616064557b42c4e89c77ced8e7ffe28cb29d8
Lim Zi Hong <lim.zihong.011s@gmail.com>
2023-04-12 14:46:02 +0800

    Should not have reverted this commit Revert "Revert "Updated files after using pylint. Yet to refactor to redesign methods with more than 5 parameters and class with more than 8 attributes.""
    This reverts commit 521c8873f6463bb8a7502cca5e8f6e8ac1dcfe13.

--- topic_modelling/non_bert/test_non_bert_topic_model.py
+++ topic_modelling/non_bert/test_non_bert_topic_model.py
@@ -1,12 +1,12 @@
-import pytest
-from sklearn.decomposition import NMF
+"""Ensure non bert topic modelling functions are working"""
 import sys
 from io import StringIO
 import os
+import pytest
+from sklearn.decomposition import NMF
 sys.path.extend([".", "../.."])
-
-from non_bert_topic_model import TopicModel
-from preprocess_class import *
+# from preprocess_class import Dataset
+from non_bert_topic_model import *
 pytestmark = pytest.mark.filterwarnings("ignore")
 
 curr_dir = os.getcwd()
@@ -16,90 +16,109 @@ if not os.path.exists(pytest_dir):
 
 @pytest.fixture
 def train_dataset():
-    df = pd.DataFrame({'Time': ['1/1/2021', '2/1/2021', '21/7/2021', '31/8/2022', '1/7/2022'], 
-                       'Sentiment': ['Positive', 'Positive', 'Positive', 'Negative', 'Negative'],
-                       'Text': ["had this cup of tea. nice tea.", "i love to have this coffee. this is my favourite coffee",
-                                "this coffee is irreplaceable. where else to find this good coffee", "this tea is too bitter. awful tea",
-                                "this coffee far too sweet. can't get olden standards coffee anymore"]})
+    """Generate train dataset"""
+    train_data = pd.DataFrame({'Time': ['1/1/2021', '2/1/2021',
+                                        '21/7/2021', '31/8/2022', '1/7/2022'],
+                       'Sentiment': ['Positive', 'Positive', 
+                                     'Positive', 'Negative', 'Negative'],
+                       'Text': ["had this cup of tea. nice tea.",
+                                "i love to have this coffee. this is my favourite coffee",
+                                "this coffee is irreplaceable. where else to find this good coffee",
+                                "this tea is too bitter. awful tea",
+                                "this coffee is too sweet. prefer olden standards coffee"]})
 
-    return Dataset(df)
+    return Dataset(train_data)
 
 @pytest.fixture
 def test_dataset():
-    df = pd.DataFrame({'Time': ['9/4/2022', '4/5/2021', '13/5/2021', '18/3/2022', '23/4/2021'],
+    """Generate test dataset"""
+    test_data = pd.DataFrame({'Time': ['9/4/2022', '4/5/2021', '13/5/2021',
+                                       '18/3/2022', '23/4/2021'],
                        'Sentiment': ['Positive', 'Positive', 'Positive', 'Negative', 'Negative'],
-                       'Text': ["I fell in love with this Keurig coffee. Amazing standards!", "The milk complements the coffee well",
-                                "Tea tarik is the best in Singapore", "Where is the honey in the tea? So bland.",
+                       'Text': ["I fell in love with this Keurig coffee. Amazing standards!", 
+                                "The milk complements the coffee well",
+                                "Tea tarik is the best in Singapore", 
+                                "Where is the honey in the tea? So bland.",
                                 "Never ever buy coffee from Brazil. I dun like that after taste"]})
-    return Dataset(df)
+    return Dataset(test_data)
 
 def test_set_topic_labels(train_dataset, monkeypatch):
     """
     Ensures that topic_label attribute is updated upon running this method.
     """
-    testModel = TopicModel(train_dataset = train_dataset, custom_print = False)
+    test_model = TopicModel(train_dataset = train_dataset, custom_print = False)
     sample_input = StringIO('testTopic')
     monkeypatch.setattr('sys.stdin', sample_input)
-    testModel.set_topic_labels(0)
-    assert testModel.topic_label == ['testTopic']
+    test_model.set_topic_labels(0)
+    assert test_model.topic_label == ['testTopic']
 
 def test_get_input_text(train_dataset):
     """
     Ensures that the right set of tokens is outputted by the method.
     """
-    testModel = TopicModel(train_dataset = train_dataset, custom_print = False)
+    test_model = TopicModel(train_dataset = train_dataset, custom_print = False)
     train_dataset.word_tokenizer(lower_case = False)
-    expected_output = train_dataset.tokenized_words
-    assert testModel.get_input_text(testModel.train_dataset, 0, remove_stop_words = False).tolist() == expected_output.tolist()
+    expected_output = train_dataset.tokenized_words.tolist()
+    assert test_model.get_input_text(test_model.train_dataset,
+                                     0, remove_stop_words = False).tolist() == expected_output
 
 def test_preprocess_dataset(train_dataset):
     """
     Ensures that the vectorizer is generated based on the specified input.
     """
-    testModel = TopicModel(train_dataset = train_dataset, custom_print = False)
-    testModel.preprocess_dataset(remove_stop_words = False, include_words = [], exclude_words = [])
-    assert testModel.train_dataset.bow is not None
+    test_model = TopicModel(train_dataset = train_dataset, custom_print = False)
+    test_model.preprocess_dataset(remove_stop_words = False, include_words = [], exclude_words = [])
+    assert test_model.train_dataset.bow is not None
 
 def test_display_topics(train_dataset):
     """
     Ensures that the right number of topics and its corresponding topic key words are generated.
     """
-    testModel = TopicModel(train_dataset = train_dataset, custom_print = False)
-    testModel.preprocess_dataset(remove_stop_words = False, include_words = [], exclude_words = [])
+    test_model = TopicModel(train_dataset = train_dataset, custom_print = False)
+    test_model.preprocess_dataset(remove_stop_words = False, include_words = [], exclude_words = [])
     training = NMF(n_components = 2, init = 'nndsvd', random_state = 4263, solver = 'cd')
-    trained_model = training.fit_transform(testModel.train_dataset.bow[2])
-    dir = os.path.join(os.getcwd(), "pytest_TopicModel")
-    topic_key_words = testModel.display_topics(training.components_, testModel.train_dataset.bow[0].get_feature_names_out(), 
-                                               num_top_words = 2, train_output_path = dir, training_model = "NMF", vectorizer = testModel.train_dataset.bow)
+    trained_model = training.fit_transform(test_model.train_dataset.bow[2])
+    output_path = os.path.join(os.getcwd(), "pytest_TopicModel")
+    tokens = test_model.train_dataset.bow[0].get_feature_names_out()
+    topic_key_words = test_model.display_topics(training.components_,
+                                                tokens,
+                                                num_top_words = 2, train_output_path = output_path,
+                                                training_model = "NMF",
+                                                vectorizer = test_model.train_dataset.bow)
     assert len(topic_key_words) == 2
     assert len(topic_key_words[0]) == 2
 
 def test_train(train_dataset, monkeypatch):
     """
-    Ensures that the correct preprocessing is applied on train_dataset, model is trained and the topic_label attribute is updated correctly
+    Ensures that the correct preprocessing is applied on train_dataset,
+    model is trained and the topic_label attribute is updated correctly
     """
-    testModel = TopicModel(train_dataset = train_dataset, custom_print = False, feature_engineer = "tfidf")
-    testModel.preprocess_dataset(remove_stop_words = False, word_form = ['noun'])
+    test_model = TopicModel(train_dataset = train_dataset,
+                            custom_print = False, feature_engineer = "tfidf")
+    test_model.preprocess_dataset(remove_stop_words = False, word_form = ['noun'])
     inputs = iter(['Virtual topic 1', 'Virtual topic 2'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    testModel.train("NMF", num_of_topics = 2, num_top_words = 1, num_top_documents = 1, train_output_path = pytest_dir)
-    assert testModel.train_dataset.tfidf is not None
-    assert testModel.model is not None
-    assert testModel.topic_label == ['Virtual topic 1', 'Virtual topic 2']
-    
+    test_model.train("NMF", num_of_topics = 2, num_top_words = 1, 
+                     num_top_documents = 1, train_output_path = pytest_dir)
+    assert test_model.train_dataset.tfidf is not None
+    assert test_model.model is not None
+    assert test_model.topic_label == ['Virtual topic 1', 'Virtual topic 2']
 
 def test_predict(test_dataset):
     """
-    Ensures that the correct text is being predicted on, correct number of columns and column names are outputted 
+    Ensures that the correct text is being predicted on, correct number of columns
+    and column names are outputted 
     and the text are labelled based on the input topic labels
     """
     pickled_vectorizer = os.path.join(pytest_dir, "training_tfidf_vectorizer_2.pk")
     pickled_model = os.path.join(pytest_dir, "training_NMF_model_2.pk")
     topic_label = os.path.join(pytest_dir, "topic_key_words.csv")
-    testModel = TopicModel(test_dataset = test_dataset, pickled_vectorizer = pickled_vectorizer, pickled_model = pickled_model,
-                           topic_label = topic_label, custom_print = False, feature_engineer = "tfidf")
-    testModel.preprocess_dataset(remove_stop_words = False, word_form = ['noun'])
-    labelled_test = testModel.predict(test_output_path = pytest_dir, root_word_option = 0, remove_stop_words = False)
+    test_model = TopicModel(test_dataset = test_dataset, pickled_vectorizer = pickled_vectorizer,
+                            pickled_model = pickled_model, topic_label = topic_label,
+                            custom_print = False, feature_engineer = "tfidf")
+    test_model.preprocess_dataset(remove_stop_words = False, word_form = ['noun'])
+    labelled_test = test_model.predict(test_output_path = pytest_dir, root_word_option = 0,
+                                       remove_stop_words = False)
     output_topic_label = labelled_test['Topic_label'].unique().tolist()
     output_topic_label.sort()
     expected_topic_label = pd.read_csv(topic_label)['Topic_label'].tolist()
@@ -110,18 +129,22 @@ def test_predict(test_dataset):
 
 def test_churn_eval_metrics(test_dataset, monkeypatch):
     """
-    Ensures that the number of topics processed and the number of samples from each topic is included in the output file is correct.
+    Ensures that the number of topics processed and the number of samples
+    from each topic is included in the output file is correct.
     """
     pickled_vectorizer = os.path.join(pytest_dir, "training_tfidf_vectorizer_2.pk")
     pickled_model = os.path.join(pytest_dir, "training_NMF_model_2.pk")
     topic_label = os.path.join(pytest_dir, "topic_key_words.csv")
-    testModel = TopicModel(test_dataset = test_dataset, pickled_vectorizer = pickled_vectorizer, pickled_model = pickled_model,
-                           topic_label = topic_label, custom_print = False, feature_engineer = "tfidf")
-    testModel.preprocess_dataset(remove_stop_words = False, word_form = ['noun'])
-    labelled_test = testModel.predict(test_output_path = pytest_dir, root_word_option = 0, remove_stop_words = False)
+    test_model = TopicModel(test_dataset = test_dataset, pickled_vectorizer = pickled_vectorizer,
+                            pickled_model = pickled_model,
+                           topic_label = topic_label, custom_print = False,
+                           feature_engineer = "tfidf")
+    test_model.preprocess_dataset(remove_stop_words = False, word_form = ['noun'])
+    labelled_test = test_model.predict(test_output_path = pytest_dir, root_word_option = 0,
+                                       remove_stop_words = False)
     inputs = iter(["1", "0"])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    testModel.churn_eval_metrics(labelled_test, 1, pytest_dir)
+    test_model.churn_eval_metrics(labelled_test, 1, pytest_dir)
 
     churned_output = pd.read_csv(os.path.join(pytest_dir, "test_sample_labels.csv"))
     assert churned_output.shape[0] == 2
